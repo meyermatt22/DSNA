4be619a7993c2d6f6f03aaecd291fa8d85fef01e
Matthew (Matt) Meyer <meyermatt22@gmail.com>
2024-02-13 18:43:16 -0600

    contains duplicate

---
+++ Arrays&Hashing/ContainsDuplicate.py
@@ -0,0 +1,17 @@
+# Given an integer array nums,
+# return true if any value appears at least twice in the array,
+# and return false if every element is distinct.
+
+class Solution(object):
+    def containsDuplicate(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        newSet = set()
+
+        for n in nums:
+            if n in newSet:
+                return True
+            newSet.add(n)
+        return False
