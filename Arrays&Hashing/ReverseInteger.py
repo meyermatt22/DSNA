3b2b69be46a68a8629c512b72b4b6ba53eed39cf
Matthew (Matt) Meyer <meyermatt22@gmail.com>
2024-06-08 15:50:57 -0500

    reverseintegersol

--- Arrays&Hashing/ReverseInteger.py
+++ Arrays&Hashing/ReverseInteger.py
@@ -16,3 +16,22 @@
 
 # Input: x = 120
 # Output: 21
+
+class Solution:
+    def reverse(self, x: int) -> int:
+
+        negative = x < 0
+
+        str_x = str(abs(x))
+
+        reversed_str = str_x[::-1]
+
+        reversed_x = int(reversed_str)
+
+        if negative:
+            reversed_x *= -1
+
+        if reversed_x < -2**31 or reversed_x > 2**31 - 1:
+            return 0
+
+        return reversed_x
