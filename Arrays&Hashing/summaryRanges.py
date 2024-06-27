0fe48b87959fd94904683b42e07603601f7e7b98
Matthew (Matt) Meyer <meyermatt22@gmail.com>
2024-06-27 09:24:48 -0500

    sumrangesom

--- Arrays&Hashing/summaryRanges.py
+++ Arrays&Hashing/summaryRanges.py
@@ -8,3 +8,30 @@
 
 # "a->b" if a != b
 # "a" if a == b
+
+class Solution:
+    def summaryRanges(self, nums: List[int]) -> List[str]:
+        ranges = []
+        if not nums:
+            return ranges
+
+        start = nums[0]
+        end = nums[0]
+
+        for i in range(1, len(nums)):
+            if nums[i] == end + 1:
+                end = nums[i]
+            else:
+                if start == end:
+                    ranges.append(str(start))
+                else:
+                    ranges.append(f"{start}->{end}")
+                start = nums[i]
+                end = nums[i]
+
+        if start == end:
+            ranges.append(str(start))
+        else:
+            ranges.append(f"{start}->{end}")
+
+        return ranges
