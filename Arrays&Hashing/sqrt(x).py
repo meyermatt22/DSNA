d813475d4677f352251bc3ecf63d41c3cfecf086
Matthew (Matt) Meyer <meyermatt22@gmail.com>
2024-06-29 21:33:57 -0500

    swrt

--- Arrays&Hashing/sqrt(x).py
+++ Arrays&Hashing/sqrt(x).py
@@ -2,7 +2,6 @@
 
 # You must not use any built-in exponent function or operator.
 
-# For example, do not use pow(x, 0.5) in c++ or x ** 0.5 in python.
 
 class Solution:
     def mySqrt(self, x: int) -> int:
