e454b9067712d9517bcec2c527a5034f02535eff
Matthew (Matt) Meyer <meyermatt22@gmail.com>
2024-07-17 22:11:25 -0500

    3sum

---
+++ Arrays&Hashing/3sum.py
@@ -0,0 +1,37 @@
+# Given an integer array nums, return all the triplets [nums[i], nums[j], nums[k]] such that i != j, i != k, and j != k, and nums[i] + nums[j] + nums[k] == 0.
+
+# Notice that the solution set must not contain duplicate triplets.
+
+from typing import List
+
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        nums.sort()
+        n = len(nums)
+        result = []
+
+        for i in range(n-2):
+            if i > 0 and nums[i] == nums[i-1]:
+                continue
+
+            left = i + 1
+            right = n - 1
+
+            while left < right:
+                curr_sum = nums[i] + nums[left] + nums[right]
+                if curr_sum == 0:
+                    result.append([nums[i], nums[left], nums[right]])
+
+                    while left < right and nums[left] == nums[left+1]:
+                        left += 1
+
+                    while left < right and nums[right] == nums[right-1]:
+                        right -= 1
+                    left += 1
+                    right -= 1
+                elif curr_sum < 0:
+                    left += 1
+                else:
+                    right -= 1
+
+        return result
